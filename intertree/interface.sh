70662335b2b1d4008d1242b7c036376f1c5c8b78
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2024-09-24 18:18:08 +0200

    Add files via upload

---
+++ intertree/interface.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Welcome To InterTree"
+echo "Interface For PythonTree Library"
+echo "Using The Tree Libary For Python"
+echo "tree = Run Tree"
+echo "exit = Exit the program"
+
+while true; do
+    # Prompt the user for a command
+    read -p ">>> " command
+
+    # Check if the user wants to run the tree command
+    if [ "$command" = "tree" ]; then
+        python main.py
+        continue  # Continue to the next iteration after running the command
+    fi
+
+    # Check if the user wants to exit
+    if [ "$command" = "exit" ]; then
+        echo "Exiting the mini terminal."
+        break
+    fi
+
+    # Execute any other command
+    eval "$command"
+done
+
